bc9ea904a4c441007a8c03077082621d70f6a319
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-12 20:47:59 -0300

    increments feature

--- advancedObjects.js
+++ advancedObjects.js
@@ -23,3 +23,14 @@ robot.checkEnergy();
 
 //Privacy
 
+const robot = {
+    _energyLevel: 100,
+    recharge(){
+      this._energyLevel += 30;
+      console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+    }
+};
+  
+robot._energyLevel = 'high';
+  
+robot.recharge(30);
