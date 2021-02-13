8ffcfc99a92a6b8c587f4a5dab967da8558b0be6
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-13 20:01:22 -0300

    Merger develop

--- numberSum.js
+++ numberSum.js
@@ -1,1 +1,50 @@
+<<<<<<< HEAD
 // Sum  const let 
+=======
+#Bracket Notation
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  // Write your code below
+  let isActive = spaceship ['Active Mission'];
+
+  console.log(isActive);
+
+  #Property Assignment
+
+  let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  // Write your code below
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 7;
+  delete spaceship['Secret Mission'];
+
+  #Methods
+
+  let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+>>>>>>> develop
