f87b0a89d406f1d27837257d12a475b6898e6a64
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-14 15:59:55 -0300

    Built-in Object Methods

--- advancedObjects.js
+++ advancedObjects.js
@@ -124,4 +124,18 @@ const robot = {
 const {functionality} = robot
 robot.functionality.beep()
 
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+const robotKeys = Object.keys(robot);
+console.log(robotKeys);
+
+const robotEntries = Object.entries(robot);
+console.log(robotEntries);
+
 //
