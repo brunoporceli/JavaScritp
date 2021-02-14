2383ebaab07acbfa5fb0d897633b922067c72f8a
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-14 15:26:41 -0300

    Destructured Assignment

--- advancedObjects.js
+++ advancedObjects.js
@@ -76,4 +76,36 @@ const robot = {
 robot.numOfSensors = 100;
 console.log(robot.numOfSensors);
 
-//
+//Factory Functions
+
+const robotFactory = (model, mobile) => {
+  return { 
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    } 
+  }
+};
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
+
+//Property Value Shorthand
+
+function robotFactory(model, mobile){
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+//Destructured Assignment
+
