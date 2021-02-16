2e3b206d6275711b266d81fe933b9627a64c2f72
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-16 18:02:45 -0300

    Methods

--- Classes.js
+++ Classes.js
@@ -35,3 +35,21 @@ class Surgeon {
 
 //Instance
 
+class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
+  
+  const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+  
+  console.log(surgeonRomero.name);
+  console.log(surgeonRomero.department);
+  
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+  
+  console.log(surgeonJackson.name);
+  console.log(surgeonJackson.department);
+
+  //
