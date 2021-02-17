3a2fde1487326ebbe1585dfc4f721640661a345f
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-17 19:07:08 -0300

    Method with get daysOff

--- Classes.js
+++ Classes.js
@@ -60,19 +60,25 @@ class Surgeon {
       this._department = department;
       this._remainingVacationDays = 20;
     }
-    get name(){
+    
+    get name() {
       return this._name;
     }
-    get department(){
+    
+    get department() {
       return this._department;
     }
-    get remainingVacationDays(){
+    
+    get remainingVacationDays() {
       return this._remainingVacationDays;
     }
-    takeVacationDays(){
-      this.remainingVacationDays(dayOff);
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
     }
   }
   
   const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
   const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
+  //
