5dab73d42f8045a5c2b352efab84a2a63d5f7e3f
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-20 21:16:50 -0300

    import

--- IntermediateJavaScriptModules.js
+++ IntermediateJavaScriptModules.js
@@ -49,3 +49,12 @@ export default Airplane;
 
 //import
 
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
