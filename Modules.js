957bbe133b4292b12bb41d2e185d932d6b1675a9
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-23 15:07:19 -0300

    Named exports

--- Modules.js
+++ Modules.js
@@ -60,3 +60,27 @@ function displayFuelCapacity() {
 displayFuelCapacity();
 
 //Named Exports
+
+let availableAirplanes = [
+  {
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
+  },
+  {
+    name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: ['pilots', 'flightAttendants']
+  }
+];
+
+let flightRequirements = {requiredStaff: 4};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true
+  } else {
+    return false
+  }
+  export {availableAirplanes, flightRequirements, meetsStaffRequirements};
+};
