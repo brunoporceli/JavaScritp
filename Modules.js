d585f720dcbdca1e9b7ecbd3cb058b642b60e30d
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-23 19:35:24 -0300

    Export Named Exports

--- Modules.js
+++ Modules.js
@@ -87,3 +87,22 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
 
 //Named Imports
 
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+//Export Named Exports
