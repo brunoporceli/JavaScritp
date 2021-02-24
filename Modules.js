7ac965c50201d491de349cfc541b8aaec72afb8f
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-24 18:03:43 -0300

    export function

--- Modules.js
+++ Modules.js
@@ -106,3 +106,41 @@ function displayStaffStatus() {
 displayStaffStatus();
 
 //Export Named Exports
+
+export let availableAirplanes = [{
+  name: 'AeroJet',
+  maxSpeed: 1200,
+  minSpeed: 300,
+  fuelCapacity: 800,
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500,
+  maxSpeed: 800,
+  minSpeed: 200,
+  availableStaff: ['pilots', 'flightAttendants']
+ }];
+ 
+ export let flightRequirements = {
+   requiredStaff: 4,
+   requiredSpeedRange: 700
+ };
+ 
+ export function meetsStaffRequirements(availableStaff, requiredStaff) {
+   if (availableStaff.length >= requiredStaff) {
+     return true;
+   } else {
+     return false;
+   }
+ };
+ 
+ export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+   let range = maxSpeed - minSpeed;
+   if (range > requiredSpeedRange){
+     return true
+   } else {
+     return false
+   }
+ };
+ 
+ export { availableAirplanes, flightRequirements, meetsStaffRequirements};
