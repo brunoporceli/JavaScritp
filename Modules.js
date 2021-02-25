31953f515ee9783ae27dff4e32e4e8fda55f7643
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-02-25 20:03:39 -0300

    dual import

--- Modules.js
+++ Modules.js
@@ -289,3 +289,29 @@ export {availableAirplanes as aircrafts, flightRequirements as flightReqs, meets
 
 //Combining Import Statements
 
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsStaffRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
