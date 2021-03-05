fa83fb611bd1246d78e07903061aaff6eeefcb34
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-05 17:13:50 -0300

    Promises with Functione and reject in the functioThe Node setTimeout() Functionn

--- Promises.js
+++ Promises.js
@@ -27,3 +27,15 @@ console.log(orderPromise);
 
 //The Node setTimeout() Function
 
+console.log("This is the first line of code in app.js.");
+
+const usingSTO = () => {
+  console.log("This line of code will log to the console last.");
+};
+setTimeout(usingSTO, 100);
+
+console.log("This is the last line of code in app.js.");
+
+////node <name_file>.js
+
+//
