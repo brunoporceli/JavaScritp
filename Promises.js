3b2ccb77e3dcb0ed2d560d672f65c99e8d1a7b5f
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-05 18:13:22 -0300

    Success and Failure Callback Functions

--- Promises.js
+++ Promises.js
@@ -41,3 +41,18 @@ console.log("This is the last line of code in app.js.");
 //Consuming Promises
 ////Success and Failure Callback Functions
 
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+ 
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+ 
+checkInventory(order).then(handleSuccess, handleFailure);
+
+//
