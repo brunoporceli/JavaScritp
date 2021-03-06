74d959fb938538231b17e57c5a902aab715fb308
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-06 18:54:14 -0300

    Avoiding Common Mistakes

--- Promises.js
+++ Promises.js
@@ -94,3 +94,23 @@ checkInventory(order)
 .catch((errorMessage) => {
   console.log(errorMessage);
 });
+
+//Avoiding Common Mistakes
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+});
