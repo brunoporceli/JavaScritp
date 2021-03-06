d7ea2c6c3c9aae29eb4af736cf89cbcb67441fc6
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-06 18:14:29 -0300

    Using catch() with Promises

--- Promises.js
+++ Promises.js
@@ -56,3 +56,20 @@ checkInventory(order).then(handleSuccess, handleFailure);
 
 //Using catch() with Promises
 
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+checkInventory(order)
+	.then(handleSuccess)
+	.catch(handleFailure);
+
+
