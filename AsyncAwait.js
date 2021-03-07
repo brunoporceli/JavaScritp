b08d7c24846e85207ad79d7328f2f33ab2eee056
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-07 15:10:06 -0300

    ASYNC AWAIT

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,1 +1,34 @@
+//ASYNC AWAIT
 
+const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
+
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  let firstSentence = data;
+  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
+    if (err) throw err;
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence);
+  });
+});
+
+let firstSentence;
+promisifiedReadfile('./file.txt', 'utf-8')
+  .then((data) => {
+    firstSentence = data;
+    return promisifiedReadfile('./file2.txt', 'utf-8');
+  })
+  .then((data) => {
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  })
+  .catch((err) => {console.log(err)});
+
+async function readFiles() {
+  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8');
+  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
+  console.log(firstSentence, secondSentence);
+}
+
+readFiles();
